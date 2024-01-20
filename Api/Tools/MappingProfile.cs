f772862ffed6eb538accac340bbae1596234a2f2
devMichaelREdwards <72928278+devMichaelREdwards@users.noreply.github.com>
2024-01-19 19:49:50 -0500

    Fix mapping issue

--- Api/Tools/MappingProfile.cs
+++ Api/Tools/MappingProfile.cs
@@ -6,5 +6,7 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<Employee, EmployeeDto>();
+        CreateMap<EmployeePosition, string>().ConvertUsing(p => p.Title);
+        CreateMap<EmployeeStatus, string>().ConvertUsing(p => p.Title);
     }
 }
