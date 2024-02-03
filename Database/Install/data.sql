5e0c2d820380d4cb33b7266c31c92e958e17e03d
devMichaelREdwards <72928278+devMichaelREdwards@users.noreply.github.com>
2024-02-02 23:39:11 -0500

    Create default value SQL files

---
+++ Database/Install/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO "CustomerStatus" ("CustomerStatusId", "Title")
+VALUES (1, 'ACTIVE'),
+    (2, 'BANNED');
+INSERT INTO "EmployeePosition" ("EmployeePositionId", "Title")
+VALUES (1, 'Employee'),
+    (2, 'Owner');
+INSERT INTO "EmployeeStatus" ("EmployeeStatusId", "Title")
+VALUES (1, 'ACTIVE'),
+    (2, 'TERMINATED');
+INSERT INTO "IngredientCategory" ("IngredientCategoryId", "Title")
+VALUES (1, 'Mushroom'),
+    (2, 'Plant'),
+    (3, 'Butterfly'),
+    (4, 'Stone'),
+    (5, 'Gem');
+INSERT INTO "OrderStatus" ("OrderStatusId", "Title")
+VALUES (1, 'OPEN'),
+    (2, 'REJECTED'),
+    (3, 'COMPLETED'),
+    (4, 'IN PROGRESS');
