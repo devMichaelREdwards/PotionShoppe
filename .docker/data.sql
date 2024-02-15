7a8c9537e80070aa4a720efead55f7aa665d2d82
devMichaelREdwards <72928278+devMichaelREdwards@users.noreply.github.com>
2024-02-15 15:52:53 -0500

    Add more test data

--- .docker/data.sql
+++ .docker/data.sql
@@ -2,23 +2,71 @@ INSERT INTO "Effect" (
         "Name",
         "Value",
         "Duration",
-        "Description"
+        "Description",
+        "Color"
     )
-VALUES ('Minor Health', 25, 0, 'Regens {value} health'),
-    ('Major Health', 100, 0, 'Regens {value} health'),
-    ('Minor Mana', 25, 0, 'Regens {value} mana'),
-    ('Major Mana', 100, 0, 'Regens {value} mana'),
+VALUES (
+        'Minor Health',
+        25,
+        0,
+        'Regens {value} health',
+        'red'
+    ),
+    (
+        'Major Health',
+        100,
+        0,
+        'Regens {value} health',
+        'red'
+    ),
+    (
+        'Minor Mana',
+        25,
+        0,
+        'Regens {value} mana',
+        'lightblue'
+    ),
+    (
+        'Major Mana',
+        100,
+        0,
+        'Regens {value} mana',
+        'darkblue'
+    ),
     (
         'Defense',
         50,
         10,
-        'Increases defense by {value} for {duration} minutes'
+        'Increases defense by {value} for {duration} minutes',
+        'grey'
     ),
     (
         'Acid Protection',
         0,
         10,
-        'Grants Acid Protection for {duration} minutes'
+        'Grants Acid Protection for {duration} minutes',
+        'limegreen'
+    ),
+    (
+        'Strength Boost',
+        0,
+        120,
+        'Increases physical strength for {duration} seconds.',
+        'Red'
+    ),
+    (
+        'Invisibility',
+        0,
+        60,
+        'Turns the user invisible for {duration} seconds.',
+        'Purple'
+    ),
+    (
+        'Wisdom Enhancement',
+        0,
+        180,
+        'Enhances magical abilities for {duration} seconds.',
+        'Gold'
     );
 INSERT INTO "Ingredient" (
         "Name",
@@ -30,19 +78,59 @@ INSERT INTO "Ingredient" (
         "EffectId",
         "IngredientCategoryId"
     )
-VALUES (
-        'Test Ingredient',
-        'Bla bla bla',
-        0,
-        0,
-        0,
-        'Test',
-        1,
+VALUES ('Mana Wing', '', 50, 20, 10, '', 4, 3),
+    ('King''s Crown', '', 1000, 700, 2, '', 5, 5),
+    ('Elf''s Ear', '', 50, 30, 100, '', 6, 2),
+    (
+        'Mystic Mushroom',
+        'A rare mushroom with magical properties.',
+        10,
+        5,
+        50,
+        'https://localhost:7211/api/Image/Mystic_Mushroom.png',
+        9,
+        1
+    ),
+    (
+        'Moonlight Herb',
+        'A herb bathed in the light of the moon.',
+        8,
+        4,
+        75,
+        'https://localhost:7211/api/Image/Moonlight_Herb.png',
+        8,
+        2
+    ),
+    (
+        'Enchanted Butterfly Wing',
+        'A butterfly wing with mystical energy.',
+        15,
+        8,
+        30,
+        'https://localhost:7211/api/Image/Enchanted_Butterfly_Wing.png',
+        3,
+        3
+    ),
+    (
+        'Crystalized Earth Stone',
+        'A stone infused with earth magic.',
+        12,
+        6,
+        40,
+        'https://localhost:7211/api/Image/Crystalized_Earth_Stone.png',
+        5,
         4
     ),
-    ('Mana Wing', '', 50, 20, 10, '', 4, 3),
-    ('King''s Crown', '', 1000, 700, 2, '', 5, 5),
-    ('Elf''s Ear', '', 50, 30, 100, '', 6, 2);
+    (
+        'Sapphire Gem',
+        'A precious gem with water affinity.',
+        20,
+        10,
+        20,
+        'https://localhost:7211/api/Image/Sapphire_Gem.png',
+        8,
+        5
+    );
 INSERT INTO "Potion" (
         "Name",
         "Description",
@@ -56,11 +144,44 @@ VALUES ('Potion Of Health', '', 0, 0, 5, '', 1),
     ('Mushroom Mind', '', 0, 0, 5, '', 1),
     ('Wizard''s Rest', '', 0, 0, 5, '', 1),
     ('Anti-Acid Coating', '', 0, 0, 5, '', 1),
-    ('Potion Of The Golem', '', 0, 0, 5, '', 1);
+    ('Potion Of The Golem', '', 0, 0, 5, '', 1),
+    (
+        'Elixir of Vitality',
+        'A potion that restores health and enhances strength.',
+        30,
+        15,
+        20,
+        'https://localhost:7211/api/Image/Elixir_of_Vitality.png',
+        1
+    ),
+    (
+        'Mana Crystal Brew',
+        'Brewed from rare ingredients, replenishes mana.',
+        25,
+        12,
+        25,
+        'https://localhost:7211/api/Image/Mana_Crystal_Brew.png',
+        1
+    ),
+    (
+        'Phantom Draught',
+        'Grants invisibility and enhances magical abilities.',
+        40,
+        20,
+        15,
+        'https://localhost:7211/api/Image/Phantom_Draught.png',
+        1
+    );
 INSERT INTO "PotionEffect" ("PotionId", "EffectId")
 VALUES (1, 2),
     (2, 4),
     (3, 4),
     (3, 5),
     (4, 6),
-    (5, 5);
+    (5, 5),
+    (6, 7),
+    (6, 9),
+    (7, 8),
+    (7, 4),
+    (8, 8),
+    (8, 9);
