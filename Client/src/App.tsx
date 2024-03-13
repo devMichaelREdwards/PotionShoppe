9b3f52703e55f49a0bc35129c6990e140de4c592
devMichaelREdwards <72928278+devMichaelREdwards@users.noreply.github.com>
2024-03-13 13:39:54 -0400

    test-commit

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,7 +1,6 @@
 import AdminRoutes from './components/routes/AdminRoutes';
 
 const App = () => {
-    // At some point, replace /admin with something loaded from an environment file
     return (
         <>
             <AdminRoutes />
