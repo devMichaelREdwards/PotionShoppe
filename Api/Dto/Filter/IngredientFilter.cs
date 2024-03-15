e84afd462b20352ed75fb6d86caf354f60aa16c5
devMichaelREdwards <72928278+devMichaelREdwards@users.noreply.github.com>
2024-03-15 11:16:59 -0400

    Ingredients pricing and stock filters

--- Api/Dto/Filter/IngredientFilter.cs
+++ Api/Dto/Filter/IngredientFilter.cs
@@ -8,6 +8,11 @@ public partial class IngredientFilter : IFilter<Ingredient>
     public string? Name { get; set; }
     public int? IngredientCategoryId { get; set; }
     public int? EffectId { get; set; }
+    public int? CostMin { get; set; }
+    public int? CostMax { get; set; }
+    public int? PriceMin { get; set; }
+    public int? PriceMax { get; set; }
+    public int? InStock { get; set; }
     public dynamic? GetValue(string key)
     {
         switch (key)
@@ -15,6 +20,11 @@ public partial class IngredientFilter : IFilter<Ingredient>
             case "name": return Name;
             case "category": return IngredientCategoryId;
             case "effect": return EffectId;
+            case "cmin": return CostMin;
+            case "cmax": return CostMax;
+            case "pmin": return PriceMin;
+            case "pmax": return PriceMax;
+            case "instock": return InStock;
             default: return null;
         }
 
@@ -28,7 +38,12 @@ public partial class IngredientFilter : IFilter<Ingredient>
         {
             Name = ParseFilter.GetString("name", query),
             IngredientCategoryId = ParseFilter.GetInt("category", query),
-            EffectId = ParseFilter.GetInt("effect", query)
+            EffectId = ParseFilter.GetInt("effect", query),
+            CostMin = ParseFilter.GetInt("cmin", query),
+            CostMax = ParseFilter.GetInt("cmax", query),
+            PriceMin = ParseFilter.GetInt("pmin", query),
+            PriceMax = ParseFilter.GetInt("pmax", query),
+            InStock = ParseFilter.GetInt("instock", query)
         };
     }
 }
