9d1150df8122e7c04540f890392b11da5dd8777c
devMichaelREdwards <72928278+devMichaelREdwards@users.noreply.github.com>
2024-04-24 05:05:02 -0400

    Add color picker and submit to effect form

--- Client/src/components/admin/forms/EffectForm.tsx
+++ Client/src/components/admin/forms/EffectForm.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import ActionButton from '../../common/input/ActionButton';
-import { Form } from 'rsuite';
-import { NumberControl, TextAreaControl, TextControl } from '../../common/input/FormControl';
+import { Form, Message, useToaster } from 'rsuite';
+import { ColorPickerControl, NumberControl, TextAreaControl, TextControl } from '../../common/input/FormControl';
+import axios from '../../../api/axios';
+import useAuth from '../../../hooks/useAuth';
 
 interface IProps {
     editId: number;
@@ -13,10 +15,54 @@ const EffectForm = ({ editId, toggleEdit }: IProps) => {
     const [value, setValue] = useState('');
     const [duration, setDuration] = useState('');
     const [description, setDescription] = useState('');
+    const [color, setColor] = useState('#ff0000');
+    const toaster = useToaster();
+    const { user } = useAuth();
     useEffect(() => {
         //console.log(editId);
+        // Populate edit form here
     });
-    const handleSubmit = () => {};
+    const handleSubmit = async () => {
+        if (editId > 0) {
+            const data = {
+                effectId: editId,
+                name,
+                value,
+                duration,
+                description,
+                color,
+            };
+            await axios
+                .put('effect', data, user?.authConfig)
+                .then(() => {
+                    // Put successful
+                    toaster.push(<Message type='success'>Effect Saved</Message>, { duration: 5000 });
+                })
+                .catch((error) => {
+                    // Put failed
+                    toaster.push(<Message type='error'>{error.response.data}</Message>, { duration: 5000 });
+                });
+        } else {
+            const data = {
+                name,
+                value,
+                duration,
+                description,
+                color,
+            };
+
+            await axios
+                .post('effect', data, user?.authConfig)
+                .then((res) => {
+                    // Post successful
+                    toaster.push(<Message type='success'>Effect Saved</Message>, { duration: 5000 });
+                })
+                .catch((error) => {
+                    // Post failed
+                    toaster.push(<Message type='error'>An error occurred! Please check the form and try again.</Message>, { duration: 5000 });
+                });
+        }
+    };
     return (
         <Form fluid>
             <Form.Group>
@@ -67,12 +113,31 @@ const EffectForm = ({ editId, toggleEdit }: IProps) => {
                     }}
                 />
             </Form.Group>
+            <Form.Group>
+                <ColorPickerControl
+                    value={color}
+                    label='Color'
+                    name='color'
+                    onChange={(e: string) => {
+                        setColor(e);
+                    }}
+                />
+            </Form.Group>
+
             <ActionButton
                 appearance='ghost'
                 label='Back to list'
                 color='violet'
                 action={() => {
                     toggleEdit(false);
+                }}
+            />
+            <ActionButton
+                appearance='ghost'
+                label='Submit'
+                color='green'
+                action={() => {
+                    handleSubmit();
                 }}
             />
         </Form>
